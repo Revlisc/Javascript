08ee9e4317a303b9180f9c5883edb1e0d2d30a54
dales <dalesimmons0210@gmail.com>
2021-07-23 22:19:36 -0700

    finished

--- etchSketch/script.js
+++ etchSketch/script.js
@@ -1,18 +1,41 @@
 let container = document.querySelector('.container');
 let defaultGrid = 16;
 let reset = document.querySelector('.resetBtn');
-reset.addEventListener('click', clear);
+let resize = document.querySelector('.resize');
+let boxes;
+let grid = prompt("How large of a drawing area do you want?");
+makeGrid(grid);
 
-/*
-const square = document.querySelectorAll('.box');
-square.addEventListener("hover", () => {
-    square.style.backgroundColor = 'black';
-})
-*/
-window.onload = function(){start()};
+function makeGrid(size = defaultGrid) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-function start() {
-    let grid = prompt("How large of a grid would you like, from 1 to 99?");
+    for (let i = 1; i <= size * size; i++) {
+        let row = document.createElement('div');
+        row.classList.add('boxes');
+        container.appendChild(row);
+    };   
+    box = document.querySelectorAll(".boxes");
+    box.forEach((boxes) => {
+        boxes.addEventListener("mouseover", () => {
+            boxes.classList.add("changed");
+        });
+    });
+    
+    
+}
+
+reset.addEventListener('click', () => {
+    box.forEach((boxes) => {
+        boxes.classList.remove("changed");
+    });
+});
+
+resize.addEventListener("click", () => {
+    box.forEach((boxes) => {
+        boxes.classList.remove("changed");
+    });
+    grid = prompt("How large of a grid would you like, from 1 to 99?");
     if (grid === NaN) {
         alert("You must pick a number.");
     }
@@ -20,47 +43,4 @@ function start() {
         grid === 99;
     }
     makeGrid(grid);
-}
-
-function makeGrid(size = defaultGrid) {
-
-    for (let i = 1; i <= size; i++) {
-        let row = document.createElement('div');
-        row.classList.add('row');
-        container.appendChild(row);
-        for (let j = 1; j <= size; j++) {
-            const box = document.createElement('div');
-            box.classList.add("box");
-            box.addEventListener("mouseover", () => {
-                alterColor(box);
-            });
-            row.appendChild(box);
-            
-        }
-    }
-    
-}
-
-function alterColor(box) {
-    box.classList.add("changed");
-}
-
-function clear() {
-    let grid = container.querySelectorAll('div');
-    grid.forEach(grid => grid.style.backgroundColor = 'white');
-    container.empty();
-    start();
-}
-
-/*
-const square = document.querySelectorAll('.box');
-square.forEach((box) => {
-    square.addEventListener("mouseover", () => {
-        alterColor(box);
-    })
-});  
-*/  
-
-
-
-//makeGrid(25);
+});
